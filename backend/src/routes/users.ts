12abd71e40b5376aef6819ba6ecc1073fb316fa5
JatinSharma222 <sharma.jatin2206@gmail.com>
2026-09-13 15:20:46 +0530

    feat: Added loop back login to get balance using another queue

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -3,154 +3,180 @@ import jwt from "jsonwebtoken";
 import { eq } from "drizzle-orm";
 import { createClient } from "redis";
 
-const QUEUE_NAME = "queue-" + Math.random().toString().substring(0, 5);
+import { db } from "../db";
+import { users } from "../db/schema";
+
+import { authMiddleware, JWT_SECRET, type AuthRequest } from "../middleware";
+
+import type {
+  Claims,
+  DepositRequest,
+  OnRampRequest,
+  OrderRequest,
+  SigninInput,
+  SignupInput,
+  SignupResponse,
+} from "../types/user";
 
 const client = createClient();
 client.connect();
 
 const receiveClient = createClient();
 
-import {
-    authMiddleware,
-    JWT_SECRET,
-    type AuthRequest
-} from "../middleware";
-
-import type {
-    Claims,
-    DepositRequest,
-    OnRampRequest,
-    OrderRequest,
-    SigninInput,
-    SignupInput,
-    SignupResponse,
-} from "../types/user";
-
-import { db } from "../db";
-import { users } from "../db/schema";
+const QUEUE_NAME = "queue-" + Math.random().toString().substring(0, 5);
+const CALLBACKS: Record<string, (balance: unknown) => void> = {};
 
 export const router = Router();
 
-
-// const SOL_ORDERBOOK = new Ordebook("sol");
-
-// function getStockBalance(userId: number, asset: string) {
-//     return stockBalances.get(userId)!.get(asset) ?? {
-//         available: 0,
-//         locked: 0
-//     };
-// }
-
-
-
 router.post("/signup", async (req, res) => {
-    const body = req.body as SignupInput;
+  const body = req.body as SignupInput;
 
-    const existingUser = await db
-        .select()
-        .from(users)
-        .where(eq(users.username, body.username))
-        .limit(1);
+  const existingUser = await db
+    .select()
+    .from(users)
+    .where(eq(users.username, body.username))
+    .limit(1);
 
-    if (existingUser.length > 0) {
-        res.status(409).json({
-            message: "User already exists"
-        } satisfies SignupResponse);
-
-        return;
-    }
-
-    const [user] = await db
-        .insert(users)
-        .values({
-            username: body.username,
-            password: body.password
-        })
-        .returning();
-
-    if (!user) {
-        res.status(500).json({
-            message: "Failed to create user"
-        } satisfies SignupResponse);
-
-        return;
-    }
-
-    res.status(201).json({
-        message: "Successfully signed up"
+  if (existingUser.length > 0) {
+    res.status(409).json({
+      message: "User already exists",
     } satisfies SignupResponse);
+
+    return;
+  }
+
+  const [user] = await db
+    .insert(users)
+    .values({
+      username: body.username,
+      password: body.password,
+    })
+    .returning();
+
+  if (!user) {
+    res.status(500).json({
+      message: "Failed to create user",
+    } satisfies SignupResponse);
+
+    return;
+  }
+
+  res.status(201).json({
+    message: "Successfully signed up",
+  } satisfies SignupResponse);
 });
 
-
-
 router.post("/signin", async (req, res) => {
-    const body = req.body as SigninInput;
+  const body = req.body as SigninInput;
 
-    const [user] = await db
-        .select()
-        .from(users)
-        .where(eq(users.username, body.username))
-        .limit(1);
+  const [user] = await db
+    .select()
+    .from(users)
+    .where(eq(users.username, body.username))
+    .limit(1);
 
-    if (!user) {
-        res.status(401).json({
-            message: "Incorrect credentials"
-        } satisfies SignupResponse);
+  if (!user) {
+    res.status(401).json({
+      message: "Incorrect credentials",
+    } satisfies SignupResponse);
 
-        return;
-    }
+    return;
+  }
 
-    if (user.password !== body.password) {
-        res.status(401).json({
-            message: "Incorrect credentials"
-        } satisfies SignupResponse);
+  if (user.password !== body.password) {
+    res.status(401).json({
+      message: "Incorrect credentials",
+    } satisfies SignupResponse);
 
-        return;
-    }
+    return;
+  }
 
-    const claims: Claims = {
-        sub: user.id,
-        exp: Math.floor(Date.now() / 1000) + 24 * 60 * 60
-    };
+  const claims: Claims = {
+    sub: user.id,
+    exp: Math.floor(Date.now() / 1000) + 24 * 60 * 60,
+  };
 
-    const token = jwt.sign(claims, JWT_SECRET);
+  const token = jwt.sign(claims, JWT_SECRET);
 
-    res.json({
-        token
-    });
+  res.json({
+    token,
+  });
 });
 
 router.post("/onramp", authMiddleware, async (req: AuthRequest, res) => {
-    const body = req.body as OnRampRequest;
+  const body = req.body as OnRampRequest;
 
-    await client.lPush("engine-queue", JSON.stringify({
-        type: "onramp",
-        payload: {
-            userId: req.userId!,
-            amount: body.qty
-        }
-    }))
+  await client.lPush(
+    "engine-queue",
+    JSON.stringify({
+      type: "onramp",
+      payload: {
+        userId: req.userId!,
+        amount: body.qty,
+      },
+    }),
+  );
 
-    res.json({
-        message: "Onramp request received"
-    });
-
+  res.json({
+    message: "Onramp request received",
+  });
 });
 
 router.post("/deposit", authMiddleware, async (req: AuthRequest, res) => {
-    const body = req.body as DepositRequest;
+  const body = req.body as DepositRequest;
 
-    await client.lPush("engine-queue", JSON.stringify({
-        type: "Deposit",
-        payload: {
-            userId: req.userId!,
-            ticker: body.ticker,
-            qty: body.qty
-        }
-    }))
+  await client.lPush(
+    "engine-queue",
+    JSON.stringify({
+      type: "Deposit",
+      payload: {
+        userId: req.userId!,
+        ticker: body.ticker,
+        qty: body.qty,
+      },
+    }),
+  );
 
-    res.json({
-        message: "Deposit request received"
-    });
+  res.json({
+    message: "Deposit request received",
+  });
+});
 
+router.get("/balance", authMiddleware, async (req: AuthRequest, res) => {
+  const callbackId = Math.random().toString().substring(0, 5);
+  await client.lPush(
+    "engine-queue",
+    JSON.stringify({
+      type: "get_balance",
+      payload: {
+        userId: req.userId!,
+      },
+      queue: QUEUE_NAME,
+      callbackId,
+    }),
+  );
+
+  const balance = await new Promise((resolve) => {
+    CALLBACKS[callbackId] = resolve;
+  });
+
+  res.json(balance);
+});
+
+receiveClient.connect().then(async () => {
+  while (1) {
+    const res = await receiveClient.blPop(QUEUE_NAME, 1000);
+    console.log("reading from queue");
+    if (!res) {
+      continue;
+    }
+    const parsedData = JSON.parse(res.element);
+    const callbackId = parsedData.callbackId;
+    const balance = parsedData.balance;
+    const callback = CALLBACKS[callbackId];
+    if (callback) {
+      callback(balance);
+      delete CALLBACKS[callbackId];
+    }
+  }
 });
--- engine/index.ts
+++ engine/index.ts
@@ -56,5 +56,13 @@ client.connect()
                 stockBalances.get(userId)!.get(ticker)!.available += qty;
             }
 
+            if(parsedData.type == "get_balance") {
+                const userId = parsedData.payload.userId;
+                const balance = usdBalances.get(userId) || { available: 0, locked: 0 };
+                
+                const returnQueue = parsedData.queue;
+
+                sendClient.lPush( returnQueue, JSON.stringify({ balance: balance, callbackId: parsedData.callbackId}));
+            }
         }
-    })
+    });
